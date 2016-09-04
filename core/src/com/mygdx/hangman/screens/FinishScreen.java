7885ea4db36ba651d59ba4d56721e4f5fbbe4fab
Gladias <alek.drozdz@gmail.com>
2016-09-04 20:47:26 +0200

    no message

--- core/src/com/mygdx/hangman/screens/FinishScreen.java
+++ core/src/com/mygdx/hangman/screens/FinishScreen.java
@@ -1,17 +1,53 @@
 package com.mygdx.hangman.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.mygdx.hangman.HangmanGame;
 
-public class FinishScreen extends AbstractScreen{
+public class FinishScreen extends AbstractScreen {
+
+	public BitmapFont font;
+	private SpriteBatch batch;
+
 
 	public FinishScreen(HangmanGame game) {
 		super(game);
 	}
-	
+
 	@Override
 	protected void init() {
-		// TODO Auto-generated method stub
+		batch = new SpriteBatch();
+		initFonts();
+
+	}
+
+	private void initFonts() {
+		FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter params = new FreeTypeFontGenerator.FreeTypeFontParameter();
+
+		params.size = 60;
+		params.color = Color.BLACK;
+		font = generator.generateFont(params);
+
+	}
+
+	public void render(float delta) {
+		super.render(delta);
+		update();
 		
+		batch.begin();
+		
+		font.draw(batch,"xD",100,100);
+		
+		batch.end();
+	} 
+	private void update() {
+		stage.act();
+
 	}
 
 }
